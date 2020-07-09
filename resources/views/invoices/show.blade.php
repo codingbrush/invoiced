38045faf3cda2659d4c0503c7f1d52be146263d8
aeon501 <rasengan501@gmail.com>
2020-07-09 06:15:13 +0000

    invoice view refactor

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -8,16 +8,32 @@
     <title>Invoice </title>
     <style>
         .bg-orange{
-            background-color:   #FF6347 ;
+            background-color:  #FD761A ;
         }
+            @media print
+    {    
+        .no-print, .no-print *
+        {
+            display: none !important;
+        }
+    }
     </style>
 </head>
 <body class="bg-white">
-    <div class="container mx-auto p-5">
+    <div class="container mx-auto p-5 mt-5">
+        <div class="row mb-3 flex justify-between">
+            <a href="{{route('invoice.index')}}" class="px-3 py-2 rounded text-white bg-blue-700 hover:bg-blue-500 pointer no-print">&lArr;  BACK</a>
+            <button class="px-3 py-2 rounded text-white bg-green-600 hover:bg-green-400 pointer no-print" onclick="window.print();return false;">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-printer" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 2H5a1 1 0 0 0-1 1v2H3V3a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h-1V3a1 1 0 0 0-1-1zm3 4H2a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h1v1H2a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1z"/>
+                    <path fill-rule="evenodd" d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM5 8a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H5z"/>
+                    <path d="M3 7.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                  </svg></button>
+        </div>
     <section class="flex justify-between mb-3">
         @foreach($invoices as $company)
         <div class="w-40 h-30 rounded">
-        <img src="{{asset('images\/')}}{{$company->setting->logo}}" alt="deklegacylogo">
+        <img src="{{asset('images/'.$company->setting->logo)}}" alt="deklegacylogo">
         </div>
         <div class="justify-center self-center">
             
@@ -84,7 +100,7 @@
                     <td class="p-2 border">
                         <p class="float-right px-3">Sub Total: </p>
                     </td>
-                    <td class="border">
+                    <td class="border pl-4">
                         {{$invoice->subtotal}}
                     </td>
                 </tr>
@@ -94,7 +110,7 @@
                     <td class="p-2 border">
                         <p class="float-right px-3">Grand Total: </p>
                     </td class="border">
-                    <td class="border">
+                    <td class="border pl-4">
                         {{$invoice->grand_total}}
                     </td>
                 </tr>
